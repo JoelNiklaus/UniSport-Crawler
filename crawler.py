ae53f76af34e0ac0ce93dad5c4898f2221010eb3
Luca Liechti <luca.liechti@gmail.com>
2017-11-07 16:05:15 +0100

    minor work

--- crawler.py
+++ crawler.py
@@ -1,38 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
-import html5lib
+#import lxml
+#import html5lib
 import json
 from collections import OrderedDict
 
+#all courses
+#from here, visit each sport and from there, each course in tabs "Geleitetes Angebot" and "Geleitetes Training"
+all_courses = 'http://www.unibe.ch/universitaet/campus__und__infrastruktur/universitaetssport/sportangebot/sport_a_z/index_ger.html'
 
+#courses found above look like this:
 starting_url = 'http://www.zssw.unibe.ch/usp/zms/angebot/7428/index_ger.html'
-
 
 def scrape(data):
     soup = BeautifulSoup(data, 'lxml') # alternatives are 'html5lib' or html.parser
-
+    soup.unicode
     course = soup.table
-
-    print(course.prettify())
-    print(course.get_text())
-
     return toJSON(course)
 
-
-
 def toJSON(data):
-    table_data = [[cell.text for cell in row("td")] for row in data("tr")]
-    print(table_data)
-    json_data = json.dumps(OrderedDict(table_data))
-    print(json_data)
-    return json_data
+    table_data = [[cell.get_text() for cell in row("td")] for row in data("tr")]
+    #json_data = json.dumps(OrderedDict(table_data))
+    thiscourse = json.dumps(OrderedDict(table_data), sort_keys=False, indent=4).decode('unicode-escape') #now prints nicely
+    return thiscourse
 
 try:
     r = requests.get(starting_url)
     if r.status_code == 200:
-        scrape(r.content)
-
+        coursedata = scrape(r.content)
+        print coursedata
     else:
         print('Could not load content of ' + starting_url)
 except requests.exceptions.ConnectionError as connErr:
@@ -40,5 +36,3 @@ except requests.exceptions.ConnectionError as connErr:
     print(connErr)
 except Exception as e:
     print(e)
-
-
