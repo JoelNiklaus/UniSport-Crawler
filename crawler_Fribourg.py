1f122c28c1f3567e625ee9d3ff2e616a7d0bc568
Luca Liechti <luca.liechti@gmail.com>
2017-11-10 23:32:36 +0100

    significantly advanced the FRI crawler

--- crawler_Fribourg.py
+++ crawler_Fribourg.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 import requests
 #import lxml
 #import html5lib
@@ -8,26 +9,49 @@ from collections import OrderedDict
 #all courses
 #from here, visit each sport and from there, each course
 all_courses = 'http://www3.unifr.ch/sportuni/de/sportangebot/angebot-nach-aktivitaet.html'
+courseprefix = 'http://www3.unifr.ch/sportuni/de/'
 
 #courses found above look like this:
 #starting_url = 'http://www.zssw.unibe.ch/usp/zms/angebot/7428/index_ger.html'
 
-def scrape(data):
+#visit each sport page individually.
+#compile list of all sports in distinctsports.
+def scrapeMainPage(data):
     soup = BeautifulSoup(data, 'lxml') # alternatives are 'html5lib' or html.parser
-    soup.unicode
-    course = soup.table
-    return toJSON(course)
+    sports = soup.find_all(class_='link-list')
+    sportsaslist = []
+    for lst in sports:
+        thislist = lst.find_all('a')
+        for item in thislist:
+            sportsaslist.append(item)
+    for item in sportsaslist:
+        link = courseprefix + item.get('href')
+        req = requests.get(link)
+        if req.status_code == 200:
+            scrapeOneCourse(req.content.decode('utf-8'))
+        else:
+            print('Could not load content of ' + link)
 
-def toJSON(data):
-    table_data = [[cell.get_text() for cell in row("td")] for row in data("tr")]
-    #json_data = json.dumps(OrderedDict(table_data))
-    thiscourse = json.dumps(OrderedDict(table_data), sort_keys=False, indent=4)#.decode('unicode-escape') #now prints nicely
-    return thiscourse
+def scrapeOneCourse (data):
+    soup = BeautifulSoup(data, 'html5lib')
+    print(soup.h2.get_text().strip()) #title of the sport
+    print()
+    print(soup.find_all(class_='tabcordion')) #most of the info is here; we also have to follow the link in the javascript.onclick event
+
+
+
+
+
+#def toJSON(data):
+#    table_data = [[cell.get_text() for cell in row("td")] for row in data("tr")]
+#    #json_data = json.dumps(OrderedDict(table_data))
+#    thiscourse = json.dumps(OrderedDict(table_data), sort_keys=False, indent=4)
+#    return thiscourse
 
 try:
     r = requests.get(all_courses)
     if r.status_code == 200:
-        coursedata = scrape(r.content.decode('utf-8'))
+        scrapeMainPage(r.content.decode('utf-8'))
     else:
         print('Could not load content of ' + all_courses)
 except requests.exceptions.ConnectionError as connErr:
